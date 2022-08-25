ba82c43236e6fd7cb7c7f08b95d78e210cfd625d
Amelia Good <amelia.good@airbnb.com>
2022-08-25 08:48:22 -0700

    symmetrical loc dep trees

--- trees.js
+++ trees.js
@@ -8,19 +8,17 @@ let maxThickMult = 20.0;
 let startThickMult = 0.8;
 let taperStop = 0.5;
 let taperThickMult = 7;
-let horizTaperMult = 1;
 
 let branchMaxThickMult = 0.9;
 let newBranchMaxThickMult = 1.1;
 
 let mysteryHeightMult = 100;
 
-let trueDxMult = 1;
 let velVar = 0.25;
 let accVar = 0.01;
 let velMult = 0.7;
 let accMult = 0.7;
-let maxVelPow = 1;
+let maxVelPow = 2;
 
 let taperingMult = 0.95;
 let taperVar = 0.05;
@@ -35,11 +33,18 @@ let maxVel = 3;
 class Tree {
   constructor(x, y, c, width, height) {
     this.ogX = x;
+    this.ogY = y;
     this.height = height;
     this.width = width;
     this.dy = height/ dyMult;
     this.maxThick = height/maxThickMult;
-    this.branches = [new Branch(x, y, this.maxThick * startThickMult, this.maxThick, c, 0, this.dy, this.height)];
+    this.branches = [new Branch(
+      x, y, 
+      this.maxThick * startThickMult, this.maxThick,
+      c, 
+      0, this.dy,
+      this.height,
+      this.ogX, this.ogY)];
 
     minThick = height/minThickMult;
     maxVel = width/250;
@@ -58,7 +63,7 @@ class Tree {
         // taper if thinner than... (or too many branches)
         let taperThick = taperThickMult * (this.height/branch.y);
         let centerDist = abs(branch.x - this.ogX);
-        taperThick *= (1 + centerDist**horizTaperMult/this.width);
+        taperThick *= (1 + centerDist/this.width);
         if (branch.thickness < taperThick || this.branches.length > 100) {
           branch.draw();
           branch.step(true);
@@ -74,16 +79,20 @@ class Tree {
         if (branch.thickness > maxThick) {
           branch.thickness /= 2;
           branch.maxThick = max(branch.maxThick * branchMaxThickMult, branch.thickness*newBranchMaxThickMult);
+          let ogX = branch.x+branch.run()/2
           this.branches.push(
             new Branch(
-              branch.x+branch.run()/2,
-              branch.y+branch.rise()/2, 
+              ogX,
+              branch.y+branch.rise()/2+1, 
               branch.thickness,
               branch.maxThick,
               branch.color,
               branch.dx,
               branch.dy,
-              this.height));
+              this.height,
+              branch.x,
+              this.ogY));
+          branch.ogX = branch.x;
           branch.x -= branch.run()/2;
           branch.y -= branch.rise()/2;
         }
@@ -93,7 +102,9 @@ class Tree {
 }
 
 class Branch {
-  constructor(x, y, thickness, maxThick, color, dx, dy, height) {
+  constructor(x, y, thickness, maxThick, color, dx, dy, height, ogX, ogY) {
+    this.ogX = ogX;
+    this.ogY = ogY;
     this.x = x;
     this.dx = dx;
     this.ddx = 0;
@@ -101,30 +112,38 @@ class Branch {
     this.dy = dy;
     this.thickness = thickness;
     this.maxThick = maxThick;
-    this.dThick = 0;
+    this.dThick = .1;
     this.color = color;
     this.height = height * mysteryHeightMult;
   }
 
   step(taper) {
-    this.trueDx = this.dx * Math.pow(1 - this.y/this.height, trueDxMult);
-    this.x += this.trueDx;
-    this.dx += randomGaussian(this.ddx, velVar);
+    // * Math.pow(this.curHeight()/this.height, trueDxMult);
+    this.trueDx = this.dx;
+    // this.x += this.trueDx;
+    let ogDist = this.ogX - this.x;
+    // console.log('OGDIST', ogDist);
+    if (ogDist != 0) {
+      this.x += -1 * abs(ogDist)/ogDist;
+    }
+    this.dx += 0; // randomGaussian(this.ddx, velVar);
     this.ddx += randomGaussian(0, accVar);
     this.y -= this.dy;
 
-    if (abs(this.dx) > maxVel) { // * (1 - Math.pow(1 - this.y/this.height, maxVelPow))) {
-      this.dx *= velMult;
-      this.ddx *= -accMult;
-      // this.dx -= abs(this.dx)/this.dx * .25 * this.dx;
-    }
+
+    // let wat = 1 + Math.pow(this.curHeight()/this.height, maxVelPow);
+    // if (abs(this.dx) > maxVel * wat) {
+    //   this.dx *= velMult;
+    //   this.ddx *= -accMult;
+    //   // this.dx -= abs(this.dx)/this.dx * .25 * this.dx;
+    // }
 
     if (taper) {
       this.thickness *= randomGaussian(taperingMult, taperVar);
       return;
     }
 
-    this.thickness += this.dThick;
+    this.thickness += .2; // this.dThick;
     // this.thickness += randomGaussian(0, 1.5);
     this.dThick += randomGaussian(0, thickVar);
 
@@ -132,6 +151,10 @@ class Branch {
       this.dThick = 0;
       this.thickness += 1;
     }
+  }
+
+  curHeight() {
+    return this.ogY - this.y;
   }
 
   run() {
